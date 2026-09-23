805a89c30515c99a1584306841696f00a90d4814
Abhinav Singh <abhinavsingh55849@gmail.com>
2026-09-23 23:20:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (28.21%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.py
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -1,15 +1,16 @@
 class Solution(object):
     def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
         max_wealth = 0
-        
-        for customer in accounts:
-            current_wealth = 0
-            
-            
-            for money in customer:
-                current_wealth += money
-                
+    
+        for i in range(len(accounts)):
+            current_wealth = 0  
+            for j in range(len(accounts[i])):
+                current_wealth += accounts[i][j]
+
             if current_wealth > max_wealth:
                 max_wealth = current_wealth
-                
         return max_wealth
