0abdf834bd6b28117cf7ecbe7d39799ddb39c0f6
Abhinav Singh <abhinavsingh55849@gmail.com>
2026-09-23 23:13:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (99.83%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        max_wealth = 0
+        
+        for customer in accounts:
+            current_wealth = 0
+            
+            # Har bank account ka balance manually add karo
+            for money in customer:
+                current_wealth += money
+                
+            if current_wealth > max_wealth:
+                max_wealth = current_wealth
+                
+        return max_wealth
